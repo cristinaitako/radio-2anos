097b2afed1e3bacb6f61e140014fadcaa3d53476
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2023-10-26 17:40:13 +0000

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+//crianda a função tocaSomAplauso e buscando id da tecla para tocar.
 function tocaSomaplausos    () {
     document.querySelector('som_tecla_aplausos').play();
 
 }
-document.querySelector('tecla_aplausos').onclick = tocaSomaplausos;
+//referencia constante listaDeTeclas que busca todas as classes tecla.
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
   listaDeTeclas[0].onclick = tocaSomAplausos;
