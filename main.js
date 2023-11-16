93a6a4ecdd0e72b21fbcb99d1953c43971ce3c77
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2023-11-16 17:45:39 +0000

    onkeydown

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 //laco de repetição "while"foi trocado por "for",tecla, efeito, idAudio e tocaSom.
-for (let contador = 0 < listaDeTeclas.length; contador = contador++;){
+for(let contador = 0 < listaDeTeclas.length; contador = contador++;){
   const tecla = listaDeTeclas[contador]
   const efeito = tecla.classList[1]; //busca do item 1 dos botoes 
   const idAudio = `#som_${efeito}`; // uso do template string
   tecla.onclick = function(){ //funçao anomina auxiliar ao TocaSom
     tocaSom(idAudio);
   }
-   //console.log(contador);
+  tecla.onkeydown = function (){
+    tecla.classList.add("ativa");
+  }
 }
 
